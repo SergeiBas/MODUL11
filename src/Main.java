6610f60c12d1ffabdb67f370d996191f699dcf0e
SergeiBas <sergei.bas06@gmail.com>
2023-04-19 00:50:45 +0200

    one

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        NeParSpisok neParSpisok = new NeParSpisok();
+        SortSpisZA sortSpisZA = new SortSpisZA();
+        SortArr sortArr = new SortArr();
+        RandStrimNumb randStrimNumb = new RandStrimNumb();
+        PeremishElStrim peremishElStrim = new PeremishElStrim();
+
+        String str = "1. Ivan, 2. Sasha, 3. Peter, 4. Sergei, 5. Maxim, 11. Nemaxim, 7. Artur";
+        System.out.println(neParSpisok.delParNames(str));
     }
 }
--- src/NeParSpisok.java
+++ src/NeParSpisok.java
@@ -1,2 +1,27 @@
 public class NeParSpisok {
+    public String delParNames(String names){
+        String[] arrNames = names.split(",");
+        int[] arrNumbers = new int[arrNames.length];
+        String[] arrNamesSub = new String[arrNames.length];
+        for (int i = 0; i < arrNames.length; i++) {
+            arrNumbers[i] = Integer.parseInt(arrNames[i].replaceAll("[A-z, .]",""));
+        }
+        int count = 0;
+        for (int i = 0; i < arrNames.length; i++) {
+            if((arrNumbers[i]%2) == 1){
+                arrNamesSub[count] = arrNames[i];
+                count++;
+            }
+        }
+        return String.valueOf(stringBuilder(arrNamesSub));
+    }
+    public StringBuilder stringBuilder(String[] arrNames){
+        StringBuilder result = new StringBuilder("");
+        for (String name: arrNames) {
+            if(name == null) break;
+            result.append(name);
+            result.append(",");
+        }
+        return new StringBuilder(result.substring(0, (result.length() - 1)));
+    }
 }
